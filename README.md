e449ebd51ff7da29c38e529525d9a2a77c36b50f
Oshodi Kolapo <oshodikolapo@gmail.com>
2020-02-13 13:53:30 +0100

    Added README file and changed scheduling scraping to 1hr

---
+++ README.md
@@ -0,0 +1,20 @@
+# Discoco
+
+A web application that grabs discounts from E-Commerce websites. Built using Django
+
+Find it live on [Heroku](https://floating-ridge-31699.herokuapp.com/jumia/)
+
+## E-Commerce Sites Supported
+[Jumia](www.jumia.com)
+
+## Authors
+
+* **Oshodi Kolapo**
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- jumia/scraper_tools/updater.py
+++ jumia/scraper_tools/updater.py
@@ -13,26 +13,7 @@ from . import sample
 
 def start():
     scheduler = BackgroundScheduler()
-    # day_of_week='mon-fri', hour='9', minute='30', second='10',id='task_time'
-    scheduler.add_job(sample.massive_scrape_job, 'interval', minutes=5)
-    print(f"        ------ Scheduling to scrape for another 3hrs ----\nTime at scheduling : {datetime.datetime.now()}")
+    scheduler.add_job(sample.massive_scrape_job, 'interval', minutes=60, id='Mass_ScrapingID')
+    print(f"       ------ Scheduling to scrape for another 1 hour ----\nTime at scheduling : {datetime.datetime.now()}")
     time.sleep(2)
     scheduler.start()
-
-
-"""
-
-scheduler.add_jobstore(DjangoJobStore(), "default")
-
-DjangoJobExecution.objects.delete_old_job_executions(1800000)  # Delete job executions older than n days calc in millis
-@register_job(scheduler, "interval", seconds=60 * 5, id='new03')
-def start():
-    sample.massive_scrape_job()
-    # time.sleep(random.randrange(1, 100, 1) / 100.)
-    print(f"Massive Scrape job added at {datetime.datetime.now()}!")
-
-
-register_events(scheduler)
-scheduler.start()
-print("Scheduler started!")
-"""
